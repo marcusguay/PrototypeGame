e6d33fbedd7c682bf157e73098aa330d687441d2
marcusguay <55104472+marcusguay@users.noreply.github.com>
2022-05-06 21:44:31 -0700

    Add files via upload

---
+++ Source/TestGame1/MyWheeledVehicle.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WheeledVehicle.h"
+#include "MyWheeledVehicle.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTGAME1_API AMyWheeledVehicle : public AWheeledVehicle
+{
+	GENERATED_BODY()
+	
+};
